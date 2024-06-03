215d3d16f623e67e840a8cd076215c82f792687e
Micael Miranda Inácio <micaelmiranda124@gmail.com>
2024-06-03 16:29:35 -0300

    tabela de histórico - não finalizada

--- src/modules/scoreboard/history.tsx
+++ src/modules/scoreboard/history.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { format } from "date-fns";
+import { Trash2Icon } from "lucide-react";
+
 export default function History() {
   const localHistory = localStorage.getItem("history");
   let history: Match[] = [];
@@ -5,23 +20,42 @@ export default function History() {
     history = JSON.parse(localHistory);
   }
   return (
-    <div>
-      <h2>Histórico de partidas</h2>
-      {history.map((match) => (
-        <div className="flex gap-4">
-          <p>{match.datetime.toString()}</p>
-          <p>{match.player1.name}</p>
-          <p>
-            {match.player1.games} -{" "}
-            {match.player1.points.map((partial) => partial + " | ")}
-          </p>
-          <p>{match.player2.name}</p>
-          <p>
-            {match.player1.games} -{" "}
-            {match.player2.points.map((partial) => partial)}
-          </p>
-        </div>
-      ))}
+    <div className="mt-8">
+      <h2 className="text-xl">Histórico de partidas</h2>
+      <Table className="border my-2">
+        <TableHeader>
+          <TableRow className="bg-secondary">
+            <TableHead>Data</TableHead>
+            <TableHead>Jogador 1</TableHead>
+            <TableHead>Jogador 2</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {history.map((match) => (
+            <TableRow>
+              <TableCell>{format(match.datetime, "dd/MM/yyyy")}</TableCell>
+              <TableCell>
+                {match.player1.name} - {match.player1.games}
+                {" ( "}
+                {match.player1.points.map((partial) => partial + " ")})
+              </TableCell>
+              <TableCell>
+                {match.player2.name} - {match.player2.games}
+                {" ( "}
+                {match.player2.points.map((partial) => partial + " ")})
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Button
+        variant={"outline"}
+        title="Apagar histórico"
+        className="flex gap-2"
+      >
+        <Trash2Icon />
+        Limpar
+      </Button>
     </div>
   );
 }
