2a34c213b31452abc48ba18d08f63508cb9566c3
Micael Miranda Inácio <micaelmiranda124@gmail.com>
2024-06-05 16:49:25 -0300

    Home atualizada, tabela de histórico ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 import CreateMatch from "@/modules/scoreboard/create-match";
 import History from "@/modules/scoreboard/history";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="py-6">
       <CreateMatch />
-      <History />
+      <div className="w-full flex gap-4">
+        <Image
+          src="patola.svg"
+          alt="Mascote"
+          width={295}
+          height={291}
+          className="w-1/3"
+        />
+
+        <History />
+      </div>
     </section>
   );
 }
--- src/modules/scoreboard/history.tsx
+++ src/modules/scoreboard/history.tsx
@@ -1,81 +1,112 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { format } from "date-fns";
 import { Trash2Icon } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 export default function History() {
-  const localHistory = localStorage.getItem("history");
-  let history: Match[] = [];
-  if (localHistory) {
-    history = JSON.parse(localHistory);
+  const [history, setHistory] = useState<Match[]>([]);
+  useEffect(() => {
+    const localHistory = localStorage.getItem("history");
+    if (localHistory) {
+      setHistory(JSON.parse(localHistory));
+    }
+  }, []);
+
+  function deleteHistory() {
+    Swal.fire({
+      title: `Apagar o histórico de partidas?`,
+      text: `Esta ação não poderá ser desfeita.`,
+      icon: "warning",
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirmar",
+      confirmButtonColor: "#3085d6",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("history");
+        setHistory([]);
+      }
+    });
   }
   return (
-    <div className="mt-8">
+    <div className="w-2/3">
       <h2 className="text-xl">Histórico de partidas</h2>
-      <Table className="border my-2">
-        <TableHeader>
-          <TableRow className="bg-secondary">
-            <TableHead>Data</TableHead>
-            <TableHead>Jogador</TableHead>
-            <TableHead>Sets</TableHead>
-            <TableHead>Parciais</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {history.map((match) => (
-            <TableRow key={match.datetime.toString()}>
-              <TableCell>{format(match.datetime, "dd/MM/yyyy")}</TableCell>
-              <TableCell>
-                <div className="bg-primary/30 p-1 rounded h-8 flex items-center justify-center mb-1">
-                  {match.player1.name}
-                </div>
-                <div className="bg-primary/30 p-1 rounded h-8 flex items-center justify-center">
-                  {match.player2.name}
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center mb-1">
-                  {match.player1.games}
-                </div>
-                <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center">
-                  {match.player2.games}
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className="flex gap-1 mb-1">
-                  {match.player1.points.map((partial) => (
-                    <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center">
-                      {partial}
-                    </div>
-                  ))}
-                </div>
-                <div className="flex gap-1">
-                  {match.player2.points.map((partial) => (
-                    <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center">
-                      {partial}
-                    </div>
-                  ))}
-                </div>
-              </TableCell>
+      <div className="my-2 max-h-[60vh] overflow-y-auto">
+        <Table className="border">
+          <TableHeader>
+            <TableRow className="bg-secondary">
+              <TableHead>Data</TableHead>
+              <TableHead>Jogadores</TableHead>
+              <TableHead>Sets</TableHead>
+              <TableHead>Parciais</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {history.length > 0 ? (
+              history.map((match) => (
+                <TableRow key={match.datetime.toString()}>
+                  <TableCell>{format(match.datetime, "dd/MM/yyyy")}</TableCell>
+                  <TableCell>
+                    <div className="bg-primary/30 p-1 rounded h-8 flex items-center justify-center mb-1">
+                      {match.player1.name}
+                    </div>
+                    <div className="bg-primary/30 p-1 rounded h-8 flex items-center justify-center">
+                      {match.player2.name}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center mb-1">
+                      {match.player1.games}
+                    </div>
+                    <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center">
+                      {match.player2.games}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex gap-1 mb-1">
+                      {match.player1.points.map((partial) => (
+                        <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center">
+                          {partial}
+                        </div>
+                      ))}
+                    </div>
+                    <div className="flex gap-1">
+                      {match.player2.points.map((partial) => (
+                        <div className="bg-primary/30 p-1 rounded w-8 h-8 flex items-center justify-center">
+                          {partial}
+                        </div>
+                      ))}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={7} className="h-24 text-center">
+                  Não há nenhuma partida registrada.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
       <Button
         variant={"outline"}
         title="Apagar histórico"
         className="flex gap-2"
+        onClick={() => deleteHistory()}
       >
         <Trash2Icon />
         Limpar
